89e6a9e0f7f0a1aa6b6320ca597dc55ad064db14
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-12 16:03:37 -0300

    feat: install and add cors to project

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
